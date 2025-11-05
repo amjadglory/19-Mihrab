01470504dae815a81031929e3a157f520490af62
amjadglory <alamjad211096@gmail.com>
2025-11-05 08:54:08 +0200

    fix login design and design sign up component

--- src/app/core/auth/components/login/login.ts
+++ src/app/core/auth/components/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +8,8 @@ import { FormGroup } from '@angular/forms';
   styleUrl: './login.css',
 })
 export class Login {
-  loginForm: FormGroup = new FormGroup({});
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+  });
 }
--- src/app/core/auth/components/signup/signup.ts
+++ src/app/core/auth/components/signup/signup.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './signup.css',
 })
 export class Signup {
-
+  signupForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.maxLength(3),
+      Validators.minLength(20),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+    rePassword: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+    dateOfBirth: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/(19|20)\d{2}$/),
+    ]),
+    gender: new FormControl(null, [Validators.required, Validators.pattern(/^(male|female)$/)]),
+  });
 }
