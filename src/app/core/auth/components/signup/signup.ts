64471bf6e35aa1644adb60879ae7471b0fae3557
amjadglory <alamjad211096@gmail.com>
2025-11-05 13:52:14 +0200

    re password confirmation

--- src/app/core/auth/components/signup/signup.ts
+++ src/app/core/auth/components/signup/signup.ts
@@ -1,5 +1,12 @@
 import { Component, signal, WritableSignal } from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -9,23 +16,33 @@ import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angula
 })
 export class Signup {
   signupForm: WritableSignal<FormGroup> = signal(
-    new FormGroup({
-      name: new FormControl(null, [
-        Validators.required,
-        Validators.maxLength(20),
-        Validators.minLength(3),
-      ]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
-      rePassword: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
-      dateOfBirth: new FormControl(null, [
-        Validators.required,
-        Validators.pattern(/^(\d{4})-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])$/),
-      ]),
-      gender: new FormControl(null, [Validators.required, Validators.pattern(/^(male|female)$/)]),
-    })
+    new FormGroup(
+      {
+        name: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(20),
+          Validators.minLength(3),
+        ]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+        rePassword: new FormControl(null, [Validators.required]),
+        dateOfBirth: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^(\d{4})-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])$/),
+        ]),
+        gender: new FormControl(null, [Validators.required, Validators.pattern(/^(male|female)$/)]),
+      },
+      { validators: this.rePassConfirm }
+    )
   );
   submitSignupForm() {
-    console.log(this.signupForm());
+    if (this.signupForm().valid) {
+      console.log(this.signupForm());
+    }
+  }
+  rePassConfirm(group: AbstractControl): ValidationErrors | null {
+    return group.get('password')?.value === group.get('rePassword')?.value
+      ? null
+      : { mismatch: true };
   }
 }
